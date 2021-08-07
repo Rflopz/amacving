b4812fb820221e6fb6cc68a1835b73b10fdef94f
Rafael Lopez <devrafaelflc@gmail.com>
2021-08-06 18:40:59 -0700

    toggle theme button

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,7 +1,9 @@
 <script>
     import theme from '../store/theme'
     import Social from './UI/social.svelte'
-
+    import ThemeLightDark from 'svelte-material-icons/Brightness4.svelte'
+    import IconButton from './UI/IconButton.svelte'
+    
 	const toggleTheme = () => {
 		theme.set( $theme === 'dark' ? 'light' : 'dark' )
         localStorage.theme = $theme;
@@ -13,5 +15,7 @@
 <nav class="flex items-center justify-between">
     <h1 class="text-xl text-gray-900 dark:text-gray-400 font-bold">Rafael López C.</h1>
 
-    <Social/>
+    <Social>
+        <IconButton on:click={toggleTheme} Icon={ThemeLightDark} />
+    </Social>
 </nav>
