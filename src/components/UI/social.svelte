99dea156a1169baea0bbad1ab04a352d82ee469a
Rafael Lopez <devrafaelflc@gmail.com>
2021-08-06 18:17:01 -0700

    svelte-material-icons working

---
+++ src/components/UI/social.svelte
@@ -0,0 +1,19 @@
+<script>
+    
+    import GithubIcon from 'svelte-material-icons/GithubCircle.svelte'
+    import YoutubeIcon from 'svelte-material-icons/Youtube.svelte'
+    import LinkedinIcon from 'svelte-material-icons/LinkedinBox.svelte'
+
+    import IconButton from './IconButton.svelte'
+</script>
+
+
+<div class="flex items-center justify-around ">
+            
+    <IconButton Icon={YoutubeIcon} size="34px" color="#bb0000" href="https://www.youtube.com/channel/UCmKn9VxE_4qEj8FgY_aHx_w" />
+    <IconButton Icon={LinkedinIcon} color="#0077b5" href="https://www.linkedin.com/in/rafaelflc/" />
+    <IconButton Icon={GithubIcon} size="34px" href="https://github.com/devrafael" />
+
+    <slot></slot>
+
+</div>
--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,6 +1,6 @@
 <script>
     import theme from '../store/theme'
-
+    import Social from './UI/social.svelte'
 
 	const toggleTheme = () => {
 		theme.set( $theme === 'dark' ? 'light' : 'dark' )
@@ -12,4 +12,6 @@
 
 <nav class="flex items-center justify-between">
     <h1 class="text-xl text-gray-900 dark:text-gray-400 font-bold">Rafael López C.</h1>
+
+    <Social/>
 </nav>
