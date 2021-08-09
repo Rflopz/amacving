7d691e9f5a34b7412539ae340f4d65317cfe3135
Rafael Lopez <devrafaelflc@gmail.com>
2021-08-08 19:02:34 -0700

    Starting github compoment

--- src/Components/Stadistics.svelte
+++ src/Components/Stadistics.svelte
@@ -1,13 +1,12 @@
 <script>
+    import Github from './Github.svelte';
     import GlassCard from './UI/GlassCard.svelte'
 </script>
 
 <div class="mt-5 grid md:grid-cols-2 sm:grid-cols-1 gap-4">
 
     <GlassCard title="Github" class="col-span-2" >
-        <p>
-            github content
-        </p> 
+        <Github />
     </GlassCard>
 
     <GlassCard title="LinkedIn">
