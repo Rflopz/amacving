705706859b0da5ff7875714688ad3bd52095e3c0
Rafael Lopez <devrafaelflc@gmail.com>
2021-08-26 18:52:07 -0700

    firestore working

---
+++ src/Components/Firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB2u01gytI3C7SNCKvaMX__bbQ3sbKOfmg",
+    authDomain: "rafael-cv.firebaseapp.com",
+    projectId: "rafael-cv",
+    storageBucket: "rafael-cv.appspot.com",
+    messagingSenderId: "183797962783",
+    appId: "1:183797962783:web:ff7a31e8d082a9c6fedd2a",
+    measurementId: "G-PEQRYEY4PZ"
+  };
--- src/Components/Github.svelte
+++ src/Components/Github.svelte
@@ -1,5 +1,46 @@
+<script>
+    import { firebaseConfig } from './Firebase'
+    import { initializeApp } from 'firebase/app'
+    import { getFirestore, collection, onSnapshot, getDocs, doc } from 'firebase/firestore'
+    
+    const firebase = initializeApp( firebaseConfig )
+
+    const db = getFirestore()
+
+    const ghCollection = collection( db, 'Github' )
+
+    let githubData = {}
+
+    getDocs( collection( db, 'Github' ) ).then( querySnapshot => {
+
+        querySnapshot.forEach( async doc => {
+
+            const getDataDoc = ( query, name ) => {
+                let data = {}
+                query.forEach( doc => {
+                    const dataDoc = doc.data()
+                    const newd = [];
+                    newd[name] = dataDoc
+                    data = { ...data, ...newd }
+                })
+                return data
+            }
+
+            const result = await Promise.all([
+                await getDocs( collection( db, 'Github/' + doc.id + '/Projects' ) ).then( q => getDataDoc(q, 'projects' ) ), 
+                await getDocs( collection( db, 'Github/' + doc.id + '/Commits' ) ).then( q => getDataDoc( q, 'commits' ) )
+            ]).then( docData => {
+                docData.map( doc => { 
+                    githubData = { ...githubData, ...doc }
+                })
+            })
+        })
+    })
+
+</script>
+
 <div>
     <span>
-        Github component
+        Total Commits :  { console.log( githubData ) }
     </span>
 </div>
